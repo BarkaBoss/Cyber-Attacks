30370d3c154b90e02ddbfa9492af380930ecc932
BarkaBoss <bossmajor.biii@gmail.com>
2023-02-28 23:15:00 +0100

    Connected Flask to Mysql

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, url_for, flash
+from werkzeug.utils import redirect
+from flask_mysqldb import MySQL
+from my_secrets import MySecrets
+
+app = Flask(__name__)
+app.secret_key = [MySecrets.key]
+
+app.config['MYSQL_HOST'] = MySecrets.host
+app.config['MYSQL_USER'] = MySecrets.user
+app.config['MYSQL_PASSWORD'] = MySecrets.password
+app.config['MYSQL_DB'] = MySecrets.db
+
+mysql = MySQL(app)
+
+@app.route("/")
+def home():
+  cursor = mysql.connection.cursor()
+  cursor.execute("Select * FROM attack_data_tbl")
+  data = cursor.fetchall()
+  return str(data)

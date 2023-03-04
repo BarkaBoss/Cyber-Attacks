d55a298f38b3bc3d1314f5963ea6f3225c63300c
BarkaBoss <bossmajor.biii@gmail.com>
2023-03-04 01:16:32 +0100

    Finished Attack CRUD

--- app.py
+++ app.py
@@ -16,6 +16,62 @@ mysql = MySQL(app)
 @app.route("/")
 def home():
   cursor = mysql.connection.cursor()
-  cursor.execute("Select * FROM attack_data_tbl")
+  cursor.execute("Select * FROM attack_all")
   data = cursor.fetchall()
   return render_template('index.html', attacks = data)
+
+@app.route("/insert", methods=['POST'])
+def insert():
+  if request.method == 'POST':
+    flash("Data Inserted Successfully")
+    #Form Fields
+    victim = request.form['victim']
+    location = request.form['locaton']
+    industry = request.form['industry']
+    attacker_location = request.form['attacker_location']
+    malware = request.form['malware']
+    motive = request.form['motive']
+    attack_type = request.form['attack_type']
+    sub_attack_type = request.form['sub_attack_type']
+    date_of_attack = request.form['date_of_attack']
+
+    cursor = mysql.connection.cursor()
+    cursor.execute("""INSERT INTO attacks_all (victim, location, industry, attacker_location, malware, motive, attack_type, sub_attack_type, date_of_attack) 
+    VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+    (victim, location, industry, attacker_location, malware, motive, attack_type, sub_attack_type, date_of_attack))
+    mysql.connection.commit()
+    return redirect(url_for("home"))
+
+  @app.route('/delete/<string:id_data>', methods = ['GET'])
+  def delete(id_data):
+    flash("Record deleted Successfully")
+    cursor = mysql.connection.cursor()
+    cursor.execute("DELETE FROM attacks_all WHERE id=%s", (id_data,))
+    mysql.connection.commit()
+    return redirect(url_for('Home'))
+  
+  @app.route('/update', methods = ['POST', 'GET'])
+  def update():
+    if request.method == 'POST':
+      id_data = request.form['id']
+      #Form Fields
+      victim = request.form['victim']
+      location = request.form['locaton']
+      industry = request.form['industry']
+      attacker_location = request.form['attacker_location']
+      malware = request.form['malware']
+      motive = request.form['motive']
+      attack_type = request.form['attack_type']
+      sub_attack_type = request.form['sub_attack_type']
+      date_of_attack = request.form['date_of_attack']
+
+      cursor = mysql.connection.cursor()
+      cursor.execute(""" UPDATE attacks_all SET victim = %s, location = %s, industry = %s, attacker_location = %s, malware = %s, motive = %s, attack_type = %s, sub_attack_type = %s, date_of_attack = %s
+      WHERE id = %s
+      """, (victim, location, industry, attacker_location, malware, motive, attack_type, sub_attack_type, date_of_attack, id_data))
+      flash("Record Updated Successfully")
+      return redirect(url_for'Home')
+
+
+if __name__ == "__main__":
+  app.run(debug=True)

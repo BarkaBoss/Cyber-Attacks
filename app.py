9efa4cded218f06a09ad7da8db3501d6f85dbb36
BarkaBoss <bossmajor.biii@gmail.com>
2023-04-09 18:50:42 +0100

    Added Phish Demo

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, url_for, flash, jsonify
+from flask import Flask, render_template, request, url_for, flash, jsonify, session
 from werkzeug.utils import redirect
 from flask_mysqldb import MySQL
 from my_secrets import MySecrets
+import MySQLdb.cursors
+import re
 import pickle
 
 app = Flask(__name__)
@@ -15,6 +17,8 @@ app.config['MYSQL_PASSWORD'] = MySecrets.password
 app.config['MYSQL_DB'] = MySecrets.db
 
 mysql = MySQL(app)
+
+
 
 @app.route('/graphs')
 def graphs():
@@ -55,6 +59,13 @@ def insert():
     (victim, location, industry, attacker_location, malware, motive, attack_type, sub_attack_type, date_of_attack))
     mysql.connection.commit()
     return redirect(url_for("home"))
+
+def updateSecurityStatus(email):
+   cursor = mysql.connection.cursor()
+   sql = "UPDATE accounts SET status=1 WHERE email='%s'" %email
+   #val = (email)
+   cursor.execute(sql)
+   mysql.connection.commit()
 
 @app.route('/delete/<string:id_data>', methods = ['GET'])
 def delete(id_data):
@@ -115,6 +126,156 @@ def predictPhish():
       flash("Your indicators show this is a Social Engineering attack you can add it to our database as disruptive")
 
   return redirect(url_for("home"))
+
+# http://localhost:5000/pythonlogin/ - this will be the login page, we need to use both GET and POST requests
+@app.route('/pythonlogin/', methods=['GET', 'POST'])
+def login():
+# Output message if something goes wrong...
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+                # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            session['email'] = account['email']
+
+            if account['status'] == 1:
+               flash("Account Suspended for suspicious activities, contact admin", "danger")
+               return redirect(url_for('login'))
+            else:
+               return redirect(url_for('home_user'))
+        else:
+            # Account doesnt exist or username/password incorrect
+            flash("Incorrect username/password!", "danger")
+    return render_template('auth/login.html',title="Login")
+
+
+
+# http://localhost:5000/pythinlogin/register 
+# This will be the registration page, we need to use both GET and POST requests
+@app.route('/pythonlogin/register', methods=['GET', 'POST'])
+def register():
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+                # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        # cursor.execute('SELECT * FROM accounts WHERE username = %s', (username))
+        cursor.execute( "SELECT * FROM accounts WHERE username LIKE %s", [username] )
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            flash("Account already exists!", "danger")
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            flash("Invalid email address!", "danger")
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            flash("Username must contain only characters and numbers!", "danger")
+        elif not username or not password or not email:
+            flash("Incorrect username/password!", "danger")
+        else:
+        # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (email, username, password))
+            mysql.connection.commit()
+            flash("You have successfully registered!", "success")
+            return redirect(url_for('login'))
+
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        flash("Please fill out the form!", "danger")
+    # Show registration form with message (if any)
+    return render_template('auth/register.html',title="Register")
+
+# http://localhost:5000/pythinlogin/home 
+# This will be the home page, only accessible for loggedin users
+
+@app.route('/pythonlogin/home_user')
+def home_user():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('home/home.html', username=session['username'],title="Home")
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))    
+
+
+@app.route('/pythonlogin/profile')
+def profile():
+    # Check if user is loggedin
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('auth/profile.html', email=session['email'], username=session['username'],title="Profile")
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))  
+
+@app.route('/phish', methods=['POST'])
+def phish():
+    #flash("Still working on this", "success")
+    if 'loggedin' in session:
+        url = request.form['url']
+        url_pattern = "^http:\/\/"
+        ssl = re.match(url_pattern, url)
+        anchor = re.findall("@", url)
+        having_ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', url)
+        ssl_value = 0
+        length_value = 0
+        anchor_value = 0
+        having_ip_value = 0
+        sfh = 0
+        popUPWindow = 0
+        Request_url = 0
+        web_traffic = 0
+        age_of_domain = 0
+
+        if ssl:
+            ssl_value = 1
+        else:
+            ssl_value = -1
+        
+        if len(url) < 20:
+            length_value = 1
+        else:
+            length_value = -1
+        
+        if anchor:
+            anchor_value = 1
+        else:
+            anchor_value = -1
+        
+        if having_ip:
+            having_ip_value = 1
+        else:
+            having_ip_value = -1
+
+        
+        result = phish_model.predict([[sfh, popUPWindow, ssl_value, Request_url, anchor_value, web_traffic, length_value, age_of_domain, having_ip_value]])
+        print(result[0])
+
+        if result[0] == 0:
+          print("Sorry we can't conclusively predict your attack type We need more info")
+          flash("Sorry we can't conclusively predict your attack type We need more info")
+        elif result[0] == 1:
+            print("Everything looks good based on the indicators you provided")
+            flash("Everything looks good based on the indicators you provided")
+        else:
+          print("Your indicators show this is a Social Engineering attack you can add it to our database as disruptive")
+          flash("Your indicators show this is a Social Engineering attack you can add it to our database as disruptive", "danger")
+          updateSecurityStatus(session['email'])
+
+    return render_template('home/home.html', username=session['username'],title="Home")
+ 
 
 if __name__ == "__main__":
   app.run(debug=True)
